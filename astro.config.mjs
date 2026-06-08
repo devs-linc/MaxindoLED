421bc29e4ee7c2462dcfb95fcbceb37ebc30ed12
Maxindo LED <georgev.vie@gmail.com>
2026-06-08 15:48:27 +0700

    chore: scaffold Astro + tailwind/preact/sitemap/keystatic

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [preact(), sitemap()]
+});
